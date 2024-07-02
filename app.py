315722ede42c0321fd175bc03b9ab6db859477dd
PrincewillDev <princewillfidelis1@gmail.com>
2024-07-02 03:47:11 +0100

    Store my api key in a .env file and loaded my virtual env in gitignore file

--- app.py
+++ app.py
@@ -1,46 +1,41 @@
 from flask import Flask, request, jsonify
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-WEATHER_API_KEY = '9d08841fb334f501656cf35139431582'
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
-def getVisitorIp():
-    return request.remote_addr
+def get_client_location_temp(ip_addr):
+    # Get the visitor's location
+    location = f'https://ipapi.co/{ip_addr}/json/'
+    location_response = requests.get(location)
+    location_info = location_response.json()
 
-def get_Visitor_location_temp(ip_addr):
-    try:
-        # Get the visitor location
-        location = f'https://ipapi.co/{ip_addr}/json/'
-        response = requests.get(location)
-        location_info = response.json()
-        region = location_info.get('region', 'Unknown')
-        latitude = location_info.get('latitude')
-        longitude = location_info.get('longitude')
+    city = location_info.get('city', 'Unknown')
+    latitude = location_info.get('latitude')
+    longitude = location_info.get('longitude')
 
-        # Check if latitude and longitude are available
-        if not latitude or not longitude:
-            return "NULL", "NULL"
-
-        # Get the temperature information
-        weatherReq = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={WEATHER_API_KEY}&units=metric'
-        weather_response = requests.get(weatherReq)
+    if latitude and longitude:
+        weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={WEATHER_API_KEY}&units=metric'
+        weather_response = requests.get(weather_url)
         weather_info = weather_response.json()
-        temp = weather_info['main']['temp'] if 'main' in weather_info else "Not Available"
-        
-        location = f'{region}'
-        return location, temp
-    except Exception as e:
-        print(f"Error fetching data: {e}")
-        return "Unknown", "Not available"
+        temp = weather_info['main']['temp'] if 'main' in weather_info else "Not available"
+    else:
+        temp = "Couldnt get Temp"
+    return city, temp
 
 @app.route('/api/hello', methods=['GET'])
 def home():
     visitor_name = request.args.get('visitor_name')
-    client_ip = getVisitorIp()
-    location, temperature = get_Visitor_location_temp(client_ip)
-    
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)#'X-Forwarded-For' header is useful for the real client IP in production
+
+    location, temperature = get_client_location_temp(client_ip)
+
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
-    
+
     response = {
         "client_ip": client_ip,
         "location": location,
